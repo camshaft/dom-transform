bff971138b566d9e121e8f0023fbe26bb740ce19
CamShaft <bytheway.cameron@gmail.com>
2014-04-20 23:46:11 -0600

    improve react plugin

--- react.js
+++ react.js
@@ -6,35 +6,64 @@ var React = require('react');
 var d = React.DOM;
 var mori = require('mori');
 
+/**
+ * Initialize the React plugin
+ *
+ * @return {Function}
+ */
+
 module.exports = function() {
   return function(app) {
-    var render = app.render;
-    app.render = function(name, el) {
-      var dom;
-      var AppClass = React.createClass({
+    app.renderReact = function(name, fn) {
+      var component = React.createClass({
         displayName: self.name,
+        getInitialState: function() {
+          return {tree: ''};
+        },
         render: function() {
-          return d.div(null, dom);
+          return d.div(null, toDom(this.tree));
         }
-      });
-      render.call(app, name, function(err, tree) {
+      })();
+
+      // check if a dom node was passed and mount it
+      var isDomNode = typeof fn !== 'function';
+      if (isDomNode) React.renderComponent(component, fn);
+
+      app.render(name, function(err, tree) {
         if (err) return console.error(err.stack);
-        tree = toDom(tree);
+        component.setState('tree', tree);
+
+        if (isDomNode) return;
+
+        try {
+          var str = React.renderComponentToString(component);
+        } catch(err) {
+          return fn(err);
+        };
+        fn(null, str);
       });
     };
   };
 };
 
-function toDom(elem) {
-  if (mori.is_vector(elem)) return mori.into_array(mori.map(toDom, elem));
-  var name = mori.get(elem, 'name');
-  var text = mori.get(elem, 'text');
+/**
+ * Convert a mori data structure to a react dom
+ *
+ * @param {Element} el
+ * @return {ReactDom}
+ * @TODO use the immutable checking to be more efficient in dom changes
+ */
+
+function toDom(el) {
+  if (mori.is_vector(el)) return mori.into_array(mori.map(toDom, el));
+  var name = mori.get(el, 'name');
 
   var attrs = mori.reduce_kv(function(acc, key, val) {
     acc[key] = val;
     return acc;
-  }, {}, mori.get(elem, 'attrs'));
+  }, {}, mori.get(el, 'attrs'));
 
-  if (text) return d[name](attrs, text);
-  return d[name](attrs, toDom(mori.get(elem, 'children')));
+  var children = mori.get(el, 'children');
+  if (typeof children !== 'string') children = toDom(el);
+  return d[name](attrs, children);
 }
